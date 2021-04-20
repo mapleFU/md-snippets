d458a5dabe271ebf1993c9d103ae4219e1eb8f7e
mwish <anmmscs_maple@qq.com>
2021-04-21 00:47:34 +0800

    [add] add readble fn

--- components/url_shortener/src/lib.rs
+++ components/url_shortener/src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(wrapping_int_impl)]
+#![feature(assoc_char_funcs)]
 
 /// url shortener 利用 snowflake 生成 hash, 然后取 short url
 mod storage;
@@ -23,8 +24,33 @@ pub struct UrlShortener {
     storage: Box<dyn storage::Storage>,
 }
 
+// see comment for readable_fn.
+const READBLE_LENGTH: usize = 62;
+
+/// generate readable string from `origin_hash` with `length`.
+///
+/// [48, 57] means digits.
+/// [65, 90] means alphabet lower case.
+/// [97, 192] means alphabet upper case.
 fn readable_fn(length: usize, origin_hash: u64) -> String {
-    unimplemented!()
+    let mut s = String::with_capacity(length);
+    let mut current_remain = origin_hash;
+    for _ in 0..length {
+        let current = current_remain % READBLE_LENGTH as u64;
+
+        let c: u32 = match current {
+            v_digit @ 0..=9 => 48 + v_digit as u32,
+            v_lower_alpha @ 10..=35 => v_lower_alpha as u32 - 10 + 65,
+            v_upper_alpha @ 36..=61 => v_upper_alpha as u32 - 36 + 97,
+            _ => {
+                unreachable!()
+            }
+        };
+        s.push(char::from_u32(c).unwrap());
+
+        current_remain = current_remain / READBLE_LENGTH as u64;
+    }
+    s
 }
 
 impl UrlShortener {
@@ -40,7 +66,6 @@ impl UrlShortener {
     ///
     /// TODO(mwish): Do we need to handle extra timeout.
     pub fn generate_short_url(&mut self, long_url: impl AsRef<str>) -> Result<String> {
-
         let mut t = DefaultHasher::new();
         long_url.as_ref().hash(&mut t);
         let mut base_id = Wrapping(t.finish());
@@ -67,3 +92,24 @@ pub enum Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_readable_fn() {
+        let strs = vec!["nmsl", "wsngg", "wmldsnmdez"];
+        let lengths = vec![4, 6, 8, 10];
+        for l in lengths {
+            for s in &strs {
+                let mut t = DefaultHasher::new();
+                s.hash(&mut t);
+                let hash_v = t.finish();
+                let s = readable_fn(l, hash_v);
+                assert_eq!(s.len(), l);
+                println!("{:?}", s);
+            }
+        }
+    }
+}
