c57e088ab4d3895b25fa9f901be3463eca7fd77e
mwish <anmmscs_maple@qq.com>
2021-06-01 01:54:12 +0800

    support floats

--- bin/calculus/src/main.rs
+++ bin/calculus/src/main.rs
@@ -9,4 +9,25 @@ fn main() {
             .unwrap()
     );
     println!("nmsl");
+
+    println!(
+        "{}",
+        calculus_parser::calculator::ExprParser::new()
+            .parse("22.22312")
+            .unwrap()
+    );
+
+    println!(
+        "{}",
+        calculus_parser::calculator::ExprParser::new()
+            .parse("7.321E-3")
+            .unwrap()
+    );
+
+    println!(
+        "{}",
+        calculus_parser::calculator::ExprParser::new()
+            .parse("4e-11")
+            .unwrap()
+    );
 }
