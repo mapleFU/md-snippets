a77824bf6c1d67b4f386c0ccf5f29d5a9155f361
mwish <anmmscs_maple@qq.com>
2021-05-31 23:48:03 +0800

    [add] support op/cp

--- bin/calculus/src/main.rs
+++ bin/calculus/src/main.rs
@@ -1,28 +1,41 @@
-
-#[macro_use] extern crate lalrpop_util;
+#[macro_use]
+extern crate lalrpop_util;
 
 lalrpop_mod!(pub calculator); // synthesized by LALRPOP
-
 
 #[test]
 fn calculator() {
     assert!(calculator::NumParser::new().parse("22").is_ok());
     assert!(calculator::FactorParser::new().parse("22").is_ok());
     assert!(calculator::ExprParser::new().parse("22").is_ok());
+
+    assert_eq!(22, calculator::ExprParser::new().parse("22").unwrap());
+    assert_eq!(
+        22 + 22 * 22,
+        calculator::ExprParser::new().parse("22 + 22 * 22").unwrap()
+    );
+    assert_eq!(
+        22 + 22 * 22 - 22 / 2,
+        calculator::ExprParser::new()
+            .parse("22 + 22 * 22 - 22 / 2")
+            .unwrap()
+    );
+
+    assert_eq!(
+        22 + 22 * (22 - 22) / 2,
+        calculator::ExprParser::new()
+            .parse("22 + 22 * (22 - 22) / 2")
+            .unwrap()
+    );
+    assert_eq!(
+        22,
+        calculator::ExprParser::new()
+            .parse("22 + 22 * ((((((22 - 22)))))) / 2")
+            .unwrap()
+    );
 }
 
 #[cfg(not(test))]
 fn main() {
-    let var = calculator::ExprParser::new().parse("22").unwrap();
-    println!("{}", var);
-
-    let var = calculator::ExprParser::new().parse("22 + 22").unwrap();
-    println!("{}", var);
-
-
-    let var = calculator::ExprParser::new().parse("22 + 22 * 22").unwrap();
-    println!("{}", var);
-
-    let var = calculator::ExprParser::new().parse("22 + 22 * 22 - 22 / 2").unwrap();
-    println!("{}", var);
+    println!("nmsl");
 }
