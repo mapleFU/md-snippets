00655b9ce4420ecd8686cb8c7429e70a36060dd8
mwish <anmmscs_maple@qq.com>
2021-09-06 18:07:41 +0800

    tiny benchmark

--- components/enum_compare/benches/my_benchmark.rs
+++ components/enum_compare/benches/my_benchmark.rs
@@ -1,16 +1,84 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, black_box};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
+use enum_compare::*;
+
+fn non_chunked_push_back(cnt: u64) -> NonChunkedSortKeys {
+    let mut data_vec = Vec::new();
+    for i in 0..cnt {
+        data_vec.push(NonChunkedVec {
+            content: vec![Datum::I32(i as i32), Datum::I64(i as i64)],
+        });
+    }
+
+    let mut v1 = NonChunkedSortKeys::new();
+    v1.reserve(cnt as usize);
+    for i in 0..cnt {
+        v1.append(data_vec[i as usize].clone());
+    }
+    v1
+}
+
+fn chunked_push_back(cnt: u64) -> ChunkedSortKeys {
+    let mut data_vec = Vec::new();
+    for i in 0..cnt {
+        data_vec.push(NonChunkedVec {
+            content: vec![Datum::I32(i as i32), Datum::I64(i as i64)],
+        });
+    }
+
+    let mut v1 = ChunkedSortKeys::new();
+    v1.data.push(ChunkedVec::I32Chunk(ChunkedVecSized::new()));
+    v1.data.push(ChunkedVec::I64Chunk(ChunkedVecSized::new()));
+    v1.reserve(cnt as usize);
+    for i in 0..cnt {
+        v1.append(data_vec[i as usize].clone());
+    }
+    v1
+}
+
+fn searching_data<ChunkedT: SortKeys>(data_vec: &Vec<NonChunkedVec>, searching_chunk: &ChunkedT) {
+    for dv in data_vec {
+        let x = searching_chunk.lower_bound(dv);
+        black_box(x);
     }
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+fn bench_push_back(c: &mut Criterion) {
+    let mut group = c.benchmark_group("SortKeys Inserts");
+    for i in [500u64, 1500u64].iter() {
+        group.bench_with_input(BenchmarkId::new("Unchunked", i), i, |b, i| {
+            b.iter(|| non_chunked_push_back(*i))
+        });
+        group.bench_with_input(BenchmarkId::new("Chunked", i), i, |b, i| {
+            b.iter(|| chunked_push_back(*i))
+        });
+    }
+    group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_searching_idx(c: &mut Criterion) {
+    let mut group = c.benchmark_group("SortKeys Search");
+
+    for cnt in [500u64, 1500u64].iter() {
+        let mut data_vec = Vec::new();
+        for i in 0..*cnt {
+            data_vec.push(NonChunkedVec {
+                content: vec![Datum::I32(i as i32), Datum::I64(i as i64)],
+            });
+        }
+        let non_chunked = non_chunked_push_back(*cnt);
+        let chunked = chunked_push_back(*cnt);
+        group.bench_function(BenchmarkId::new("Unchunked", cnt), |b| {
+            b.iter(|| searching_data(&data_vec, &non_chunked))
+        });
+
+        group.bench_function(BenchmarkId::new("Chunked", cnt), |b| {
+            b.iter(|| searching_data(&data_vec, &chunked))
+        });
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_push_back, bench_searching_idx);
 criterion_main!(benches);
---
+++ components/enum_compare/src/lib.rs
@@ -0,0 +1,341 @@
+use std::cmp::Ordering;
+
+#[derive(PartialEq, Clone)]
+pub enum Datum {
+    Null,
+    I64(i64),
+    I32(i32),
+    Bytes(Vec<u8>),
+}
+
+impl PartialOrd for Datum {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            (Datum::Null, Datum::Null) => Some(Ordering::Equal),
+            (Datum::Null, _) => Some(Ordering::Less),
+            (Datum::I64(i1), Datum::I64(i2)) => i1.partial_cmp(i2),
+            (Datum::I32(i1), Datum::I32(i2)) => i1.partial_cmp(i2),
+            (Datum::Bytes(i1), Datum::Bytes(i2)) => i1.partial_cmp(i2),
+            _ => unimplemented!(),
+        }
+    }
+}
+
+pub trait DatumVec {
+    fn append(&mut self, datum: Datum);
+    fn len(&self) -> usize;
+    fn delete(&mut self, pos: usize);
+    fn insert(&mut self, d: Datum);
+    fn lower_bound(&self, d: &Datum) -> usize;
+    fn upper_bound(&self, d: &Datum) -> usize;
+}
+
+#[derive(Clone, PartialOrd, PartialEq)]
+pub struct NonChunkedVec {
+    pub content: Vec<Datum>,
+}
+
+impl DatumVec for NonChunkedVec {
+    fn append(&mut self, datum: Datum) {
+        self.content.push(datum);
+    }
+
+    fn len(&self) -> usize {
+        self.content.len()
+    }
+
+    fn delete(&mut self, pos: usize) {
+        self.content.remove(pos);
+    }
+
+    fn insert(&mut self, d: Datum) {
+        let i = self.content.partition_point(|v| v < &d);
+        self.content.insert(i, d);
+    }
+
+    fn lower_bound(&self, d: &Datum) -> usize {
+        self.content.partition_point(|v| v < d)
+    }
+
+    fn upper_bound(&self, d: &Datum) -> usize {
+        self.content.partition_point(|v| v > d)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ChunkedVecSized<T: Sized> {
+    data: Vec<T>,
+}
+
+impl<T: Sized> ChunkedVecSized<T> {
+    pub fn new() -> Self {
+        ChunkedVecSized { data: vec![] }
+    }
+
+    fn len(&self) -> usize {
+        self.data.len()
+    }
+}
+
+pub enum ChunkedVec {
+    I32Chunk(ChunkedVecSized<i32>),
+    I64Chunk(ChunkedVecSized<i64>),
+    BytesChunk(ChunkedVecSized<Vec<u8>>),
+}
+
+impl ChunkedVec {
+    pub fn reserve(&mut self, sz: usize) {
+        match self {
+            ChunkedVec::I32Chunk(v) => v.data.reserve(sz),
+            ChunkedVec::I64Chunk(v) => v.data.reserve(sz),
+            ChunkedVec::BytesChunk(v) => v.data.reserve(sz),
+        }
+    }
+}
+
+impl DatumVec for ChunkedVec {
+    fn append(&mut self, datum: Datum) {
+        match (datum, self) {
+            (Datum::I32(i), ChunkedVec::I32Chunk(iv)) => {
+                iv.data.push(i);
+            }
+            (Datum::I64(i), ChunkedVec::I64Chunk(iv)) => {
+                iv.data.push(i);
+            }
+            (Datum::Bytes(i), ChunkedVec::BytesChunk(iv)) => {
+                iv.data.push(i);
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    fn len(&self) -> usize {
+        match self {
+            ChunkedVec::I32Chunk(v) => v.len(),
+            ChunkedVec::I64Chunk(v) => v.len(),
+            ChunkedVec::BytesChunk(v) => v.len(),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn delete(&mut self, pos: usize) {
+        todo!()
+    }
+
+    fn insert(&mut self, d: Datum) {
+        todo!()
+    }
+
+    fn lower_bound(&self, d: &Datum) -> usize {
+        todo!()
+    }
+
+    fn upper_bound(&self, d: &Datum) -> usize {
+        todo!()
+    }
+}
+
+pub trait SortKeys {
+    fn append(&mut self, datum: NonChunkedVec);
+    fn len(&self) -> usize;
+    fn delete(&mut self, pos: usize);
+    fn insert(&mut self, d: NonChunkedVec);
+    fn lower_bound(&self, d: &NonChunkedVec) -> usize;
+    fn upper_bound(&self, d: &NonChunkedVec) -> usize;
+}
+
+pub struct ChunkedSortKeys {
+    pub data: Vec<ChunkedVec>,
+}
+
+pub struct NonChunkedSortKeys {
+    pub data: Vec<NonChunkedVec>,
+}
+
+impl NonChunkedSortKeys {
+    pub fn new() -> Self {
+        NonChunkedSortKeys { data: vec![] }
+    }
+
+    pub fn reserve(&mut self, sz: usize) {
+        self.data.reserve(sz);
+    }
+}
+
+impl SortKeys for NonChunkedSortKeys {
+    fn append(&mut self, datum: NonChunkedVec) {
+        self.data.push(datum);
+    }
+
+    fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    fn delete(&mut self, pos: usize) {
+        self.data.remove(pos);
+    }
+
+    fn insert(&mut self, d: NonChunkedVec) {
+        let i = self.data.partition_point(|v| v < &d);
+        self.data.insert(i, d);
+    }
+
+    fn lower_bound(&self, d: &NonChunkedVec) -> usize {
+        self.data.partition_point(|v| v < d)
+    }
+
+    fn upper_bound(&self, d: &NonChunkedVec) -> usize {
+        self.data.partition_point(|v| v > d)
+    }
+}
+
+impl ChunkedSortKeys {
+    pub fn compare_idx(&self, idx: usize, other: &NonChunkedVec) -> Option<Ordering> {
+        for current_index_idx in 0..self.data.len() {
+            let result = match (
+                &other.content[current_index_idx],
+                &self.data[current_index_idx],
+            ) {
+                (Datum::I32(i), ChunkedVec::I32Chunk(iv)) => iv.data[idx].partial_cmp(i),
+                (Datum::I64(i), ChunkedVec::I64Chunk(iv)) => iv.data[idx].partial_cmp(i),
+                (Datum::Bytes(i), ChunkedVec::BytesChunk(iv)) => iv.data[idx].partial_cmp(i),
+                _ => unimplemented!(),
+            };
+            match result {
+                None => {
+                    unimplemented!()
+                }
+                Some(o) => match o {
+                    Ordering::Less => {
+                        return Some(Ordering::Less);
+                    }
+                    Ordering::Greater => {
+                        return Some(Ordering::Greater);
+                    }
+                    Ordering::Equal => {}
+                },
+            }
+        }
+        Some(Ordering::Equal)
+    }
+
+    pub fn new() -> Self {
+        ChunkedSortKeys { data: vec![] }
+    }
+
+    pub fn reserve(&mut self, sz: usize) {
+        for v in self.data.iter_mut() {
+            v.reserve(sz);
+        }
+    }
+}
+
+impl SortKeys for ChunkedSortKeys {
+    fn append(&mut self, datum: NonChunkedVec) {
+        debug_assert!(self.data.len() == datum.content.len());
+        let mut idx = 0;
+        for datum_item in datum.content {
+            self.data[idx].append(datum_item);
+            idx += 1;
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.data[0].len()
+    }
+
+    fn delete(&mut self, pos: usize) {
+        for idx in 0..self.data.len() {
+            self.data[idx].delete(pos);
+        }
+    }
+
+    fn insert(&mut self, d: NonChunkedVec) {
+        if self.data.len() != d.content.len() {
+            panic!("datum count not matches");
+        }
+        let mut idx = 0;
+        for datum_item in d.content {
+            self.data[idx].insert(datum_item);
+            idx += 1;
+        }
+    }
+
+    fn lower_bound(&self, d: &NonChunkedVec) -> usize {
+        let mut upper = self.len();
+        let mut lower = 0;
+        while upper > lower {
+            let current = lower + (upper - lower) / 2;
+            match self.compare_idx(current, d).unwrap() {
+                Ordering::Less => {
+                    lower = current + 1;
+                }
+                Ordering::Equal => {
+                    upper = current;
+                }
+                Ordering::Greater => {
+                    upper = current;
+                }
+            }
+        }
+        lower
+    }
+
+    fn upper_bound(&self, _d: &NonChunkedVec) -> usize {
+        todo!()
+    }
+}
+
+fn main() {
+    use std::time::Instant;
+
+    let mut data_vec = Vec::new();
+    for i in 0..5000 {
+        data_vec.push(NonChunkedVec {
+            content: vec![Datum::I32(i)],
+        });
+    }
+
+    let mut v1 = ChunkedSortKeys::new();
+    {
+        v1.data.push(ChunkedVec::I32Chunk(ChunkedVecSized::new()));
+        let now = Instant::now();
+        for i in 0..5000 {
+            v1.append(data_vec[i].clone());
+        }
+
+        let elapse = now.elapsed();
+        println!("d1 {:?}", elapse);
+    }
+
+    let mut v2 = NonChunkedSortKeys::new();
+    {
+        let now = Instant::now();
+        for i in 0..5000 {
+            v2.append(data_vec[i].clone());
+        }
+
+        let elapse = now.elapsed();
+        println!("d2 {:?}", elapse);
+    }
+
+    {
+        let now = Instant::now();
+        for i in 0..5000 {
+            v1.lower_bound(&data_vec[i]);
+        }
+
+        let elapse = now.elapsed();
+        println!("d1-2 {:?}", elapse);
+    }
+
+    {
+        let now = Instant::now();
+        for i in 0..5000 {
+            v2.lower_bound(&data_vec[i]);
+        }
+
+        let elapse = now.elapsed();
+        println!("d2-2 {:?}", elapse);
+    }
+}
